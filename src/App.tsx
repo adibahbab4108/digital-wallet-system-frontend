4f970fc84ae9bfee27e72839dac8f81788fb9b88
adibahbab4108 <adibalvi476@gmail.com>
2025-08-21 01:08:54 +0600

    Project initialization done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <Button>Click Me</Button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
