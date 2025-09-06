7524cef5eb144e92e1c1e653c456fb33a9e4ba74
adibahbab4108 <adibalvi476@gmail.com>
2025-09-06 21:42:44 +0600

    Profile page template added

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,10 @@
+import Heading from "@/components/modules/dashboard/Heading";
+
 export default function Profile() {
   return (
+   <>
     <div>Profile</div>
+    <Heading title="This page will be updated soon" />
+   </>
   )
 }
